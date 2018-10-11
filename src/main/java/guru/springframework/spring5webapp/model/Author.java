bb01eb594458c0476b1b86ed73b8da0bc590e90b
Henrique Vicentini <Henrique_Vicentini@gap.com>
2018-10-11 18:23:06 -0300

    Equality in Hibernate: Section 2, Lecture 14

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -1,6 +1,10 @@
 package guru.springframework.spring5webapp.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,15 +21,18 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books  = new HashSet<>();
+    private Set<Book> books = new HashSet<>();
+
 
     public Author() {
     }
+
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
 
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
@@ -33,35 +40,74 @@ public class Author {
         this.books = books;
     }
 
+
     public Long getId() {
         return id;
     }
+
 
     public void setId(Long id) {
         this.id = id;
     }
 
+
     public String getFirstName() {
         return firstName;
     }
+
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+
     public String getLastName() {
         return lastName;
     }
+
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+
     public Set<Book> getBooks() {
         return books;
     }
 
+
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+
+        Author author = (Author) object;
+
+        if (!id.equals(author.id)) return false;
+
+        return true;
+    }
+
+
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + id.hashCode();
+        return result;
+    }
+
+
+    @Override
+    public java.lang.String toString() {
+        return "Author{" +
+               "id=" + id +
+               ", firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", books=" + books +
+               '}';
     }
 }
--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -1,6 +1,12 @@
 package guru.springframework.spring5webapp.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,17 +24,20 @@ public class Book {
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
-    inverseJoinColumns = @JoinColumn(name = "author_id"))
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
+
 
     public Book() {
     }
+
 
     public Book(String title, String isbn, String publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
+
 
     public Book(String title, String isbn, String publisher, Set<Author> authors) {
         this.title = title;
@@ -37,43 +46,85 @@ public class Book {
         this.authors = authors;
     }
 
+
     public Long getId() {
         return id;
     }
+
 
     public void setId(Long id) {
         this.id = id;
     }
 
+
     public String getTitle() {
         return title;
     }
+
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+
     public String getIsbn() {
         return isbn;
     }
+
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
+
     public String getPublisher() {
         return publisher;
     }
+
 
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
 
+
     public Set<Author> getAuthors() {
         return authors;
     }
 
+
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+
+        Book book = (Book) object;
+
+        if (!id.equals(book.id)) return false;
+
+        return true;
+    }
+
+
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + id.hashCode();
+        return result;
+    }
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Book{" +
+               "id=" + id +
+               ", title='" + title + '\'' +
+               ", isbn='" + isbn + '\'' +
+               ", publisher='" + publisher + '\'' +
+               ", authors=" + authors +
+               '}';
     }
 }
